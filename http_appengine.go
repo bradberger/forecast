84a35ecc5c07d6b9890b2815ea1ff517ca3e76d4
Brad Berger <brad@bradb.net>
2016-05-25 17:43:01 +0200

    Removes unsed function

--- http_appengine.go
+++ http_appengine.go
@@ -11,12 +11,6 @@ import (
     "google.golang.org/appengine/urlfetch"
 )
 
-func gaeResponse(r *http.Request, key string, lat float64, long float64, t time.Time, units Units, lang Language) (res *http.Response, err error) {
-    ctx := appengine.NewContext(r)
-    client := urlfetch.Client(ctx)
-	return client.Get(fmt.Sprintf("%s/%f/%f?units=%s&time=%d&lang=%s", BASEURL, lat, long, units, t.Unix(), lang))
-}
-
 func Init(r *http.Request, key string, units Units, lang Language) *Client {
     return &Client{
         key: key,
